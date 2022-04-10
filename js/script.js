6ba75071234d6e5b10bb4c9557c55feb9732503d
Yaroslav_QA <yaroslav@teamate-exec.com>
2022-04-11 01:59:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+const category = 'toys';
+console.log(`https://test.com/${category}/5`);
+
+const user = "tester";
+alert(`Hello ${user}`);
+*/
+
+console.log(4 + +" - obj");
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == 8);
+console.log(2*4 == '8');
+console.log(2*4 === 8);
+console.log(2*4 === '8');
+
+const isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose);
+console.log(isChecked || !isClose);
