2d6213bb00ffe3637500d662bc751708150db12a
Yaroslav_QA <yaroslav@teamate-exec.com>
2022-04-20 04:13:17 +0300

    nested cycles and labels

--- js/my.js
+++ js/my.js
@@ -91,3 +91,37 @@ logger();
 
 const calculator = (a, b) => a + b;
 //const calculator = (a, b) => { return a + b };
+
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+let numberAsterisk = "";
+let rowsNumber = 7;
+
+for (let i = 1; i < rowsNumber; i++) {
+    for (let j = 0; j < i; j++) {
+        numberAsterisk += "*";
+    }
+    numberAsterisk += "\n";
+}
+
+console.log(numberAsterisk);
+
+
+// METKI
+metka: for (let i = 0; i < 3; i++) {
+    console.log(`First level ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue metka;
+            console.log(`Third level ${k}`);
+            
+        }
+    }
+}
